b9491a7b282c4bf29dc8534007fc4e73c7b3466f
Stas <stastolsto@gmail.com>
2023-03-22 12:17:02 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// server.js
+
+const WebSocket = require('ws');
+
+const server = new WebSocket.Server({ port: 8081 });
+
+server.on('connection', (socket) => {
+  console.log('Client connected');
+
+  // Send a welcome message to the connected client
+  socket.send('Welcome to the WebSocket server!');
+
+  // Listen for messages from the client
+  socket.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+    // Echo the message back to the client
+    socket.send(`You said: ${message}`);
+  });
+
+  // Listen for the socket to close
+  socket.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+console.log('WebSocket server is running on port 8081');
